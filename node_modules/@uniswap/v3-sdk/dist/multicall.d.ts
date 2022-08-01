eca210408702649d4ce7b801d3a6c15049c0d311
abalshakou <abalshakou@scnsoft.com>
2022-08-01 16:22:05 +0300

    remove node_modules and dist

--- node_modules/@uniswap/v3-sdk/dist/multicall.d.ts
+++
@@ -1,9 +0,0 @@
-import { Interface } from '@ethersproject/abi';
-export declare abstract class Multicall {
-    static INTERFACE: Interface;
-    /**
-     * Cannot be constructed.
-     */
-    private constructor();
-    static encodeMulticall(calldatas: string | string[]): string;
-}
--- node_modules/@uniswap/v3-sdk/dist/utils/position.d.ts
+++
@@ -1,8 +0,0 @@
-import JSBI from 'jsbi';
-export declare abstract class PositionLibrary {
-    /**
-     * Cannot be constructed.
-     */
-    private constructor();
-    static getTokensOwed(feeGrowthInside0LastX128: JSBI, feeGrowthInside1LastX128: JSBI, liquidity: JSBI, feeGrowthInside0X128: JSBI, feeGrowthInside1X128: JSBI): JSBI[];
-}
